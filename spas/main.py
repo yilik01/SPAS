5337fa7298626df08bd4fbc00d9b3095eda7b5dd
yilik01 <yilik01@gmail.com>
2014-09-20 13:35:20 +1000

    Initial Checkin

---
+++ spas/main.py
@@ -0,0 +1,17 @@
+'''
+Created on 20 Sep 2014
+
+@author: Lik
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
